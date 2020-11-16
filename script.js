5f33d85836fbc54fb1ef0ab48bab19b862e24f1e
kritak-dev <kritak.singh.che15@itbhu.ac.in>
2020-11-17 00:38:13 +0530

    Adding functionlity to the page using javascript

---
+++ script.js
@@ -0,0 +1,19 @@
+window.addEventListener(`keydown`, playSound);
+
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); 
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+
+    audio.currentTime = 0;  //rewind to start
+    audio.play();
+    key.classList.add('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return; //skip if its not transform
+    this.classList.remove('playing');
+}
